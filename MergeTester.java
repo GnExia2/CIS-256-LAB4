777ec6382e02ac8e7844fc7e8feacd06f4ea9178
Winston <winstontsia@gmail.com>
2023-09-17 16:25:40 -0700

    print list before and after sort + execution time

--- MergeTester.java
+++ MergeTester.java
@@ -7,8 +7,17 @@ public class MergeTester {
         list.append(new Products(2, "Product B", 3, 15.99));
         list.append(new Products(4, "Product D", 4, 29.99));
 
+        // Print list before
+        System.out.println("BEFORE: ");
+        list.printList();
+        
+
         // Sort the list using merge sort
+        long startTime = System.nanoTime();
         list = MergeSort.mergeSort(list);
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        double seconds = (double) elapsedTime / 1_000_000_000.0;
 
         // Verify that the list is sorted
         Node current = list.head;
@@ -22,9 +31,15 @@ public class MergeTester {
         }
 
         if (isSorted) {
+            
+            System.out.println("RESULT: ");
+            list.printList();
             System.out.println("Merge Sort Test Passed: The list is sorted.");
         } else {
             System.out.println("Merge Sort Test Failed: The list is not sorted.");
         }
+        
+        String formattedTime = String.format("%.8f", seconds);
+        System.out.println("Execution time: " + formattedTime + " seconds");
     }
 }
